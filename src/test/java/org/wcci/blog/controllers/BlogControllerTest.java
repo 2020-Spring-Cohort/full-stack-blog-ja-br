e68c54317b635ed87632991625a1c8443b86dfc5
ja-br <letstalk@jamesbrooks.tech>
2020-03-01 12:20:52 -0500

    Blog controller passing tests

---
+++ src/test/java/org/wcci/blog/controllers/BlogControllerTest.java
@@ -0,0 +1,60 @@
+package org.wcci.blog.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.Model;
+import org.wcci.blog.models.Author;
+import org.wcci.blog.models.BlogPost;
+import org.wcci.blog.models.Genre;
+import org.wcci.blog.storage.BlogStorage;
+import org.wcci.blog.storage.repositories.TagRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+public class BlogControllerTest {
+    private BlogController underTest;
+    private Model model;
+    private BlogStorage blogStorage;
+    private BlogPost testBlog;
+    private TagRepository tagRepo;
+
+    @BeforeEach
+    void setUp(){
+        blogStorage = mock(BlogStorage.class);
+        tagRepo = mock(TagRepository.class);
+        underTest = new BlogController(blogStorage, tagRepo);
+        model = mock(Model.class);
+        Genre testGenre = new Genre("Stuff");
+        Author testAuthor = new Author("Tom", "Black");
+        testBlog = new BlogPost("Title", "Yee dee doo daa", testGenre, testAuthor);
+        when(blogStorage.findBlogById(1l)).thenReturn(testBlog);
+    }
+
+    @Test
+    public void displayBlogReturnsBlogsTemplate(){
+        String result = underTest.displayBlog(1l, model);
+        assertThat(result).isEqualTo("blogs-view");
+    }
+
+    @Test
+    public void displayBlogInteractsWithDependenciesCorrectly(){
+        underTest.displayBlog(1l, model);
+        verify(blogStorage).findBlogById(1l);
+        verify(model).addAttribute("blog", testBlog);
+    }
+
+    @Test
+    public void displayBlogMappingIsCorrect() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(underTest).build();
+        mockMvc.perform(MockMvcRequestBuilders.get("/blogs/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("blogs-view"))
+                .andExpect(model().attributeExists("blog"))
+                .andExpect(model().attribute("blog", testBlog));
+    }
+}
